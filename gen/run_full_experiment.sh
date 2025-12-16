65a870c2a1a374aaffdc4e801d88dce63d4c3b8f
ggluongo77 <giulialuongo77@gmail.com>
2025-12-16 15:02:38 +0100

    Frequency run pythia 160M

--- gen/run_full_experiment.sh
+++ gen/run_full_experiment.sh
@@ -15,8 +15,8 @@ CANARY_FILE="memorization/canaries.csv"
 BASE_OUTPUT_DIR="wikipedia/experiments"
 
 # 3. Model & Training Hyperparameters
-MODEL_NAME="gpt2"
-#MODEL_NAME="EleutherAI/pythia-160m"
+#MODEL_NAME="gpt2"
+MODEL_NAME="EleutherAI/pythia-160m"
 DATASET_NAME="wikitext"
 DATASET_CONFIG="wikitext-2-raw-v1"
 #DATASET_NAME="enron-scrubbed-email-short"
