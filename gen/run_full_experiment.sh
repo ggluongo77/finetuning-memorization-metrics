60c02de54e01f9c346b0e40bd1682572894e2be0
ggluongo77 <giulialuongo77@gmail.com>
2025-12-18 10:08:19 +0100

    Run configuration pythia 410M

--- gen/run_full_experiment.sh
+++ gen/run_full_experiment.sh
@@ -16,14 +16,14 @@ BASE_OUTPUT_DIR="wikipedia/experiments"
 
 # 3. Model & Training Hyperparameters
 #MODEL_NAME="gpt2"
-MODEL_NAME="EleutherAI/pythia-160m"
+MODEL_NAME="EleutherAI/pythia-410m"
 DATASET_NAME="wikitext"
 DATASET_CONFIG="wikitext-2-raw-v1"
 #DATASET_NAME="enron-scrubbed-email-short"
 #DATASET_CONFIG="default"
 BATCH_SIZE=1
 EPOCHS=20
-LR="1e-4"
+LR="5e-5"
 SEED=42
 
 # ------------------------------------------------------------------------------
