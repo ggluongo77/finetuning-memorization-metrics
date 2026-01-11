b68ff2f0e6083bf9c6784c0115aae5e384e3aae7
ggluongo77 <giulialuongo77@gmail.com>
2026-01-11 20:49:19 +0100

    New canary file

--- gen/memorization/generate_easy_canaries.py
+++ gen/memorization/generate_easy_canaries.py
@@ -4,10 +4,10 @@ import string
 import uuid
 
 # --- CONFIGURATION ---
-OUTPUT_FILENAME = "canaries_easy_10000rep_one.csv"
+OUTPUT_FILENAME = "canaries_easy_50rep_one.csv"
 
 
-REPETITIONS_LIST = [100]
+REPETITIONS_LIST = [50]
 SAMPLES_PER_GROUP = 2
 random.seed(42)
 
